d989e178640ddc975f50a6de1d9b43fd3843de2c
Connor Verra <connorverra@gmail.com>
2025-04-18 10:17:25 -0400

    added some basic widgets

--- components/app.py
+++ components/app.py
@@ -5,6 +5,7 @@ class App(CTk):
     def __init__(self):
         super().__init__()
 
+        # appearance settings
         set_appearance_mode("dark")
         set_default_color_theme("blue")
 
@@ -12,3 +13,18 @@ class App(CTk):
         self.minsize(500, 300)
         self.maxsize(700, 500)
         self.title(f"SE Interview Project")
+
+        # widgets
+        self.title_text = CTkTextbox(master=self, fg_color="transparent", wrap=WORD, font=(None, 32),
+                                     activate_scrollbars=False)
+        self.title_text.insert("0.0", "Accuweather Weather Tracker")
+        self.title_text.configure(state="disabled")
+        self.search_bar = CTkEntry(master=self,
+                                   placeholder_text="Enter City Name")
+        self.search_button = CTkButton(master=self,
+                                       text="Search")
+
+        # placing widgets
+        self.title_text.pack()
+        self.search_bar.pack()
+        self.search_button.pack()
