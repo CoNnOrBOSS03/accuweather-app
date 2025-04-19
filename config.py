909c702063b3701cdc2b111575738655213851c3
Connor Verra <connorverra@gmail.com>
2025-04-19 14:12:48 -0400

    switched to other key

--- config.py
+++ config.py
@@ -2,5 +2,6 @@
 # this project being accessible to you through GitHub, i've hardcoded
 # and committed my API key
 
-# ACCUWEATHER_API_KEY = "A1dbsGuM2W1ycJe4CsnCm1bgcrGmQdd0"
-ACCUWEATHER_API_KEY = "Unuu1TFFGPGPAmI4ziGfuzZfdQHQ7Vve"
+ACCUWEATHER_API_KEY = "A1dbsGuM2W1ycJe4CsnCm1bgcrGmQdd0"
+# if the first key gives a 503 error, use the other one - number of API requests is limited
+# ACCUWEATHER_API_KEY = "Unuu1TFFGPGPAmI4ziGfuzZfdQHQ7Vve"
