fcd1b0cda1bb9e17f622f505bc22bb0fc097b5cf
Connor Verra <connorverra@gmail.com>
2025-04-19 00:04:29 -0400

    changed main.py: key passed into here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from components.app import App
-from api import accuweather
+from config import ACCUWEATHER_API_KEY
 
 if __name__ == '__main__':
-    app = App()
+    app = App(ACCUWEATHER_API_KEY)
     app.mainloop()
